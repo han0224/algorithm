55cee3358141e4cb3f7c47ba2e9301dfc58052b1
han0224 <dbn0224@gmail.com>
2022-11-23 16:29:22 +0900

    강의실 배정

---
+++ baekjoon/그리디 알고리즘/11000.js
@@ -0,0 +1,85 @@
+const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs
+  .readFileSync("baekjoon/예제.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const MAX_NUMBER = 10000000000;
+
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  size() {
+    return this.heap.length - 1;
+  }
+
+  getMin() {
+    return this.heap[1] ? this.heap[1] : null;
+  }
+
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+
+  heappush(value) {
+    this.heap.push(value);
+    let curIdx = this.heap.length - 1;
+    let parIdx = (curIdx / 2) >> 0;
+
+    while (curIdx > 1 && this.heap[parIdx] > this.heap[curIdx]) {
+      this.swap(parIdx, curIdx);
+      curIdx = parIdx;
+      parIdx = (curIdx / 2) >> 0;
+    }
+  }
+
+  heappop() {
+    const min = this.heap[1];
+    if (this.heap.length <= 2) this.heap = [null];
+    else this.heap[1] = this.heap.pop();
+
+    let curIdx = 1;
+    let leftIdx = curIdx * 2;
+    let rightIdx = curIdx * 2 + 1;
+
+    if (!this.heap[leftIdx]) return min;
+    if (!this.heap[rightIdx]) {
+      if (this.heap[leftIdx] < this.heap[curIdx]) {
+        this.swap(leftIdx, curIdx);
+      }
+      return min;
+    }
+
+    while (
+      this.heap[leftIdx] < this.heap[curIdx] ||
+      this.heap[rightIdx] < this.heap[curIdx]
+    ) {
+      const minIdx =
+        this.heap[leftIdx] > this.heap[rightIdx] ? rightIdx : leftIdx;
+      this.swap(minIdx, curIdx);
+      curIdx = minIdx;
+      leftIdx = curIdx * 2;
+      rightIdx = curIdx * 2 + 1;
+    }
+
+    return min;
+  }
+}
+
+const queue = new MinHeap();
+const N = Number(input[0]);
+const study = input.slice(1).map((v) => v.split(" ").map(Number));
+study.sort((a, b) => a[0] - b[0]);
+for (let i = 0; i < N; i++) {
+  if (queue.size() > 0 && queue.heap[1] <= study[i][0]) {
+    queue.heappop();
+    queue.heappush(study[i][1]);
+    continue;
+  }
+  queue.heappush(study[i][1]);
+}
+console.log(queue.size());
